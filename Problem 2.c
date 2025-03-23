a4390ad27a935c0d2cda393be17a0e8f54a44f5f
Mohammed <Pointlessovrtnkr@proton.me>
2025-03-23 10:50:57 +0200

    new committment IG

--- Problem 2.c
+++ Problem 2.c
@@ -1,13 +1,13 @@
+// codeforces link:https://codeforces.com/group/MWSDmqGsZm/contest/219158/submission/312037594
 #include <stdio.h>
 
 int main() {
   int n;
-  printf("Enter the period: ");
     scanf("%d", &n);
     int years = n / 365;
     int remaining_days = n % 365;
     int months = remaining_days / 30;
     int days = remaining_days % 30;
-    printf("The period is %d years, %d months, and %d days.\n", years, months, days);
+    printf("%d years\n%d months\n%d days\n", years, months, days);
     return 0;
 }
